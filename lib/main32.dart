30c8203370b8c5c0c8c4963092fb95879a198fb5
Aaron <457636876@qq.com>
2019-11-12 18:32:12 +0800

    no message

---
+++ lib/main32.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AlignExample(),
+    );
+  }
+}
+
+class AlignExample extends StatefulWidget {
+  @override
+  _AlignExampleState createState() => _AlignExampleState();
+}
+
+class _AlignExampleState extends State<AlignExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('对齐与相对定位（Align）'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 120.0,
+            width: 120.0,
+            color: Colors.blue[50],
+            child: Align(
+              alignment: Alignment.topRight,
+              child: FlutterLogo(
+                size: 60,
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: Container(
+              color: Colors.blue[50],
+              child: Align(
+                widthFactor: 2,
+                heightFactor: 2,
+                alignment: Alignment.topRight,
+                child: FlutterLogo(
+                  size: 60,
+                ),
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
